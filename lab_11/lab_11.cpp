874e6ec15ea50a794c1bcbed45fc51f1478a3c57
PlatoEnjoyer <kep976@mail.ru>
2025-02-01 10:59:38 +0200

    lab 11

--- lab_11/lab_11.cpp
+++ lab_11/lab_11.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cmath>
 
 
 class Node 
@@ -26,7 +26,12 @@ public:
 public:
     LinkedList()
     {
-        head = tail = NULL;
+        head = new Node(0);
+        tail = new Node(1);
+        tail->next = head;
+        tail->prev = head;
+        head->prev = tail;
+        head->next = tail;
     }
 
     ~LinkedList()
@@ -40,88 +45,68 @@ public:
     Node* push_front(int data)
     {
         Node *ptr = new Node(data);
-        ptr->next = head;
-        if (head != NULL)
-        {
-            head->prev = ptr;
-        }
-        if (tail == NULL)
-        {
-            tail = ptr;
-        }
-        head = ptr;
-
+        ptr->prev = head;
+        ptr->next = head->next;
+        head->next->prev = ptr;
+        head->next = ptr;
         return ptr;
     }
 
     Node* push_back(int data)
     {
         Node *ptr = new Node(data);
-        ptr->prev = tail;
-        if (tail != NULL)
-        {
-            tail->next = ptr;
-        }
-        if (head == NULL)
-        {
-            head = ptr;
-        }
-        tail = ptr;
-
+        ptr->next = tail;
+        ptr->prev = tail->prev;
+        tail->prev->next = ptr;
+        tail->prev = ptr;
         return ptr;
     }
 
     void pop_front()
     {
-        if (head == NULL)
+        if (head->next == tail)
         {
             return;
         }
 
-        Node* ptr = head->next; 
-        if (ptr != NULL)
-        {
-            ptr->prev == NULL;
-        }
-        else
-        {
-            tail = NULL;
-        }
-
-        delete head;
-        head = ptr;
+        Node *ptr = head->next;
+        ptr->next->prev = head;
+        head->next = ptr->next;
+        delete ptr;
     }
 
     void pop_back()
     {
-        if (tail == NULL)
+        if (head->next == tail)
         {
             return;
         }
 
         Node* ptr = tail->prev; 
-        if (ptr != NULL)
-        {
-            ptr->next == NULL;
-        }
-        else
-        {
-            head = NULL;
-        }
-
-        delete tail;
-        tail = ptr;
+        tail->prev = ptr->prev;
+        ptr->prev->next = tail;
+        delete ptr;
     }
 
     Node* getAt(int index)
     {
-        Node* ptr = head;
+        if (head->next == tail)
+        {
+            return head;
+        }
+
+        Node* ptr = head->next;
         int n = 0;
 
-        while (n != index && ptr)
+        while (n != index && ptr != tail)
         {
             ptr = ptr->next;
             n++;
+        }
+        
+        if (ptr == tail)
+        {
+            return ptr;
         }
 
         return ptr;
@@ -132,16 +117,16 @@ public:
         return getAt(index);
     }
 
-    Node* insert(int idnex, int data)
+    Node* insert(int index, int data)
     {
-        Node* right = getAt(idnex);
-        if (right == NULL)
+        Node* right = getAt(index);
+        if (!right)
         {
             return push_back(data);
         }
 
         Node* left = right->prev;
-        if (left == NULL)
+        if (left == head)
         {
             return push_front(data);
         }
@@ -158,18 +143,18 @@ public:
     void erase(int index)
     {
         Node* ptr = getAt(index);
-        if (ptr == NULL)
+        if (!ptr)
         {
             return;
         }
 
-        if (ptr->prev == NULL)
+        if (ptr->prev == head)
         {
             pop_front();
             return;
         }
 
-        if (ptr->next == NULL)
+        if (ptr->next == tail)
         {
             pop_back();
             return;
@@ -183,26 +168,132 @@ public:
         delete ptr;
     }
 
+    void print()
+    {
+        Node* ptr = head->next;
+        while (ptr != tail)
+        {
+            std::cout << ptr->data << " ";
+            ptr = ptr->next;
+        }
+    }
+
 };
+
+bool chek_palindrome_number(int n)
+{
+    if (n < 100)
+    {
+        return false;
+    }
+
+    int digits[3];
+    for (int i = 0; i < 3; i++)
+    {
+        int curr = n % 10;
+        n /= 10;
+        digits[i] = curr;
+    }
+    if (digits[0] == digits[2])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void sort_linked_list(LinkedList& lst, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (lst[j]->data > lst[j+1]->data)
+            {
+                int tmp = lst[j]->data;
+                lst[j]->data = lst[j+1]->data;
+                lst[j+1]->data = tmp;
+            }
+        }
+    }
+}
+
+bool is_prime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    if (n == 2)
+    {
+        return true;
+    }
+
+    for (int i = 2; i < std::sqrt(n) + 1; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+bool is_ends_with_zero(int num)
+{
+    if (num % 10 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 int main()
 {
     LinkedList lst;
-    lst.push_back(1);
-    lst.push_back(3);
-    lst.push_back(8);
-    lst.push_back(4);
-    lst.push_back(1);
-    lst.insert(2, 177);
-    lst.insert(20, -10);
-    lst.erase(3);
-    lst.erase(30);
+    int n;
+    std::cin >> n;
+    bool flag = false;
 
-    for(Node* ptr = lst.head; ptr != NULL; ptr = ptr->next)
+    for (int i = 0; i < n; i++)
     {
-        std::cout << ptr->data << std::endl;
+        int el;
+        std::cin >> el;
+        lst.push_back(el);
+
+        if (chek_palindrome_number(el))
+        {
+            flag = true;
+        }
     }
 
-    std::cout << lst[1] << std::endl;
+    if (flag)
+    {
+        sort_linked_list(lst, n);
+    }
+    else
+    {
+        Node* ptr = lst.head->next;
+        int i = 0;
+        while (ptr != lst.tail)
+        {
+            if (is_prime(ptr->data) || is_ends_with_zero(ptr->data))
+            {
+                lst.insert(i, ptr->data);
+                i++;
+            }
+            ptr = ptr->next;
+            i++;
+        }
+    }
+    
+    lst.print();
     return 0;
 }
