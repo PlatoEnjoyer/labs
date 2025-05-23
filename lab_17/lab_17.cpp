f4ce43f72237aff1628f90df1bf1e2867924e857
PlatoEnjoyer <kep976@mail.ru>
2025-05-24 00:11:06 +0200

    lab 17

--- lab_17/lab_17.cpp
+++ lab_17/lab_17.cpp
@@ -2,54 +2,146 @@
 #include <vector>
 #include <stdexcept>
 
+template <typename T>
+class Vector {
+    std::vector<T> data;
+public:
+    void push_back(const T& value) { data.push_back(value); }
+    T& operator[](size_t index) { return data[index]; }
+    size_t size() const { return data.size(); }
+    void insert(size_t pos, const T& value) { data.insert(data.begin() + pos, value); }
+    void erase(size_t pos) { data.erase(data.begin() + pos); }
+};
 
-
-class BitVector {
+template <>
+class Vector<bool> {
 private:
     std::vector<unsigned char> data;
-    size_t bit_count;
+    size_t bit_count = 0;
+
+    static size_t byte_index(size_t bit_pos) { return bit_pos / 8; }
+    static size_t bit_offset(size_t bit_pos) { return bit_pos % 8; }
+    static unsigned char bit_mask(size_t bit_pos) { return 1 << bit_offset(bit_pos); }
+
+    void check_bounds(size_t index) const {
+        if (index >= bit_count) {
+            throw std::out_of_range("Vector<bool> index out of range");
+        }
+    }
 
 public:
-    BitVector(): bit_count(0) {}
+    class BitProxy {
+        Vector<bool>& container;
+        size_t bit_pos;
+    public:
+        BitProxy(Vector<bool>& cont, size_t pos) : container(cont), bit_pos(pos) {}
+
+        // Преобразование в bool
+        operator bool() const {
+            return (container.data[byte_index(bit_pos)] & bit_mask(bit_pos)) != 0;
+        }
+
+        // Присваивание значения
+        BitProxy& operator=(bool value) {
+            if (value) {
+                container.data[byte_index(bit_pos)] |= bit_mask(bit_pos);
+            } else {
+                container.data[byte_index(bit_pos)] &= ~bit_mask(bit_pos);
+            }
+            return *this;
+        }
+
+        // Присваивание другого BitProxy
+        BitProxy& operator=(const BitProxy& other) {
+            *this = static_cast<bool>(other);
+            return *this;
+        }
+    };
 
     void push_back(bool value) {
-        size_t byte_index = bit_count / 8;
-        size_t bit_index = bit_count % 8;
-
-        if (byte_index >= data.size()) {
+        if (bit_count % 8 == 0) {
             data.push_back(0);
         }
 
         if (value) {
-            data[byte_index] |= (1 << bit_index);
-        }
-        else {
-            data[byte_index] &= ~(1 << bit_index);
+            data[byte_index(bit_count)] |= bit_mask(bit_count);
+        } else {
+            data[byte_index(bit_count)] &= ~bit_mask(bit_count);
         }
 
         bit_count++;
     }
 
-    bool get(size_t index) {
-        if (index >= bit_count) {
-            throw std::out_of_range("Index out of range");
+    bool operator[](size_t index) const {
+        check_bounds(index);
+        return (data[byte_index(index)] & bit_mask(index)) != 0;
+    }
+
+    BitProxy operator[](size_t index) {
+        check_bounds(index);
+        return BitProxy(*this, index);
+    }
+
+    size_t size() const { return bit_count; }
+
+    void insert(size_t pos, bool value) {
+        if (pos > bit_count) throw std::out_of_range("Insert position out of range");
+
+        push_back(false);
+
+        for (size_t i = bit_count - 1; i > pos; --i) {
+            bool prev_val = (*this)[i-1];
+            (*this)[i] = prev_val;
         }
 
-        size_t byte_index = index / 8;
-        size_t bit_index = index % 8;
-
-        return (data[byte_index] >> bit_index) & 1;
+        (*this)[pos] = value;
     }
 
-    bool operator[](size_t index) {
-        return get(index);
+    void erase(size_t pos) {
+        check_bounds(pos);
+
+        for (size_t i = pos; i < bit_count - 1; ++i) {
+            bool next_val = (*this)[i+1];
+            (*this)[i] = next_val;
+        }
+
+        bit_count--;
+
+        if (bit_count % 8 == 0 && !data.empty()) {
+            data.pop_back();
+        }
     }
 
-    size_t size() {
-        return bit_count;
-    }
-
-    void insert(size_t index, bool value) {
-        
+    void print() const {
+        std::cout << "Vector<bool> (size=" << size() << "): ";
+        for (size_t i = 0; i < size(); ++i) {
+            std::cout << ((*this)[i] ? '1' : '0');
+        }
+        std::cout << std::endl;
     }
 };
+
+int main() {
+    Vector<bool> vbool;
+
+    vbool.push_back(true);
+    vbool.push_back(false);
+    vbool.push_back(true);
+    vbool.push_back(true);
+    vbool.push_back(false);
+    vbool.print();
+
+    vbool[1] = true;
+    vbool[3] = false;
+    vbool.print();
+
+    vbool.insert(2, false);
+    vbool.print();
+
+    vbool.erase(1);
+    vbool.print();
+
+    std::cout << "Size: " << vbool.size() << std::endl;
+
+    return 0;
+}
